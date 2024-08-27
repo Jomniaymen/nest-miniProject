9d4bfeae8417e8a04764ed92cb4a6125d53482cb
Jomniaymen <amin.jomni77@gmail.com>
2024-08-27 10:25:31 +0100

    signin

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,10 +6,12 @@ import { Model } from 'mongoose';
 import { UserDto } from 'src/users/dto/user.dto';
 import { User } from 'src/users/user.schema';
 import * as bcrypt from 'bcrypt'
+import { JwtService } from '@nestjs/jwt';
+import { SigninDto } from 'src/users/dto/userSignin.dto';
 @Injectable()
 export class AuthService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
-
+  constructor(@InjectModel(User.name) private userModel: Model<User> , private jwtService: JwtService) {}
+  
   async Signup(dto: UserDto)  {
     const {name ,email,password,age,role}=dto
   const check= await this.userModel.findOne({
@@ -28,20 +30,27 @@ export class AuthService {
  return newuser;
 
   }
-  async Signin(dto: UserDto)  {
-    const {name ,email,password,age,role}=dto
+  async Signin(dtosign:SigninDto)  {
+    const {name ,email,password,role}=dtosign
   const check= await this.userModel.findOne({
     email});
     if(!check){
         throw new  UnauthorizedException('email wrong');
     }
- const passwordmatch=await bcrypt.hash(password,check.password);
+ const passwordmatch=await bcrypt.compare(password,check.password);
  if(!passwordmatch){
     throw new  UnauthorizedException('password wrong');
 }
- 
- 
-
-  
+const payload = { sub: check._id, name: check.name, role: check.role,password:check.password };
+const access_token = await this.jwtService.signAsync(payload);
+return {
+  access_token,
+  user: {
+    id: check._id,
+    name: check.name,
+    email:check.email,
+    password:check.password 
+  },
   }
+}
 }
